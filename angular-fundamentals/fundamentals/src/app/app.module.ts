71f59a05445c657db1f5a04f4ccd489f1d53c2c9
Dang Nguyen Anh Khoa <anhkhoa91280@gmail.com>
2018-10-04 15:39:49 +0700

    update way to generate courses

--- angular-fundamentals/fundamentals/src/app/app.module.ts
+++ angular-fundamentals/fundamentals/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
