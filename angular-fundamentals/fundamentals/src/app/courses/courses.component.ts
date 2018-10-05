a3d222c190318101acefd36291c3acc1be72ab6b
Dang Nguyen Anh Khoa <anhkhoa91280@gmail.com>
2018-10-06 00:22:45 +0700

    test property binding

--- angular-fundamentals/fundamentals/src/app/courses/courses.component.ts
+++ angular-fundamentals/fundamentals/src/app/courses/courses.component.ts
@@ -7,8 +7,9 @@ import { CoursesService } from '../courses.service';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-  school = "RMIT University";
-  courses;
+  school :string = "RMIT University";
+  courses : string[];
+  isDisabled : boolean = false;
   constructor(coursesService:CoursesService) {     
     this.courses = coursesService.getCourseList();  
   }
